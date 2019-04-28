20e4eed5b979b7c65700c26e7f4d01b0256c8dba
JIAPENG GUO <gjp77440169@gmail.com>
2019-04-28 07:32:43 +0000

    modified plugin `WP phpMyAdmin` version 2.3 (after activation of `Premium Addons for Elementor` version 2.6.8)

--- wp-content/plugins/wp-phpmyadmin-extension/lib/phpMyAdmin_4yGIqUkNT7YHvRo9uFhc5fa/_session_temp.php
+++ wp-content/plugins/wp-phpmyadmin-extension/lib/phpMyAdmin_4yGIqUkNT7YHvRo9uFhc5fa/_session_temp.php
@@ -1,1 +1,1 @@
-<?php $sess_vars = array("time"=>1556427849, "name"=>"wp_pma_3YdIz9WiQ8oAlV",  "value"=>"wp_pma_FP9SGXJoHBck8thYa5s4zCM",  "require_ip"=>true, "ip"=>"118.138.97.113", "strip_slashes"=>false);
+<?php $sess_vars = array("time"=>1556430995, "name"=>"wp_pma_prHoDYtvAPFSOJ",  "value"=>"wp_pma_bJIqoxrmjaT8MLXNpDU4v2V",  "require_ip"=>true, "ip"=>"118.138.178.57", "strip_slashes"=>false);
