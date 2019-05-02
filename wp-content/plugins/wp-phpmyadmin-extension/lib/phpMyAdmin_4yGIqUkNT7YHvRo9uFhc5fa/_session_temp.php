ab97accd872046873bf38a58fea39293bbdffbd0
JIAPENG GUO <gjp77440169@gmail.com>
2019-05-02 05:26:44 +0000

    modified plugin `WP phpMyAdmin` version 2.3 (after activation of `Graph Generator` version 1.0.0)

--- wp-content/plugins/wp-phpmyadmin-extension/lib/phpMyAdmin_4yGIqUkNT7YHvRo9uFhc5fa/_session_temp.php
+++ wp-content/plugins/wp-phpmyadmin-extension/lib/phpMyAdmin_4yGIqUkNT7YHvRo9uFhc5fa/_session_temp.php
@@ -1,1 +1,1 @@
-<?php $sess_vars = array("time"=>1556462842, "name"=>"wp_pma_t2kElWrwsQuNRc",  "value"=>"wp_pma_JiXFobRQ1zxPjvyMNAD7YtW",  "require_ip"=>true, "ip"=>"120.148.45.38", "strip_slashes"=>false);
+<?php $sess_vars = array("time"=>1556773805, "name"=>"wp_pma_UB6rgjvTSCyhlG",  "value"=>"wp_pma_JmdsbqZRYM2Kc7lfhSzFy1T",  "require_ip"=>true, "ip"=>"118.138.97.113", "strip_slashes"=>false);
